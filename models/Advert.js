c3043e3f194e00f053236e0f7fe6a4db355c4bf5
HKW875 <hkw875@hotmail.com>
2026-04-26 12:01:37 +0300

    Update Advert.js

--- models/Advert.js
+++ models/Advert.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
 
+const AdSchema = new mongoose.Schema({
+    category: String,
+    title: String,
+    price: Number,
+    description: String,
+    locationName: String,
+    phone: String,
+    condition: { type: String, enum: ['new', 'used'] },
+    images: [String],
+    geo: {
+        lat: Number,
+        lng: Number
+    },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Advert', AdSchema);
